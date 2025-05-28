f254bfe7168e465ff7a64b92bc20245b70fec527
bryercowan <cowan224@gmail.com>
2025-05-27 20:55:24 -0500

    feat: added 60fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        t = clock.tick(60)
+        dt = t/1000
 
 if __name__ == "__main__":
     main()
